7ad1e2b10d0dce592989f549bb39e5750c349fbd
Shashank <shashank232106@gmail.com>
2026-05-02 18:10:41 +0530

    fix: add secure and sameSite cookie options for cross-domain auth

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -40,7 +40,11 @@ async function registerUserController(req,res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token )
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+    })
 
     res.status(201).json({
         message:"User registered successfully",
@@ -82,7 +86,12 @@ async function loginUserController(req,res) {
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
     )
-    res.cookie("token", token )
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+ })
+
     res.status(200).json({
         message:"User logged in successfully",
         user:{
@@ -108,7 +117,12 @@ async function logoutUserController(req,res){
         await tokenBlacklistModel.create({token})
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+    })
+    
     res.status(200).json({
         message:"User logged out successfully"
     })
