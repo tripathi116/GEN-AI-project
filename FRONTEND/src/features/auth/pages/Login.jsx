d739b0223951d6d95198c184b030d04592fdef9e
Shashank <shashank232106@gmail.com>
2026-05-03 15:23:39 +0530

    fix: redirect to login after register and home after login

--- FRONTEND/src/features/auth/pages/Login.jsx
+++ FRONTEND/src/features/auth/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     const handleSubmit =  async (e) => {
         e.preventDefault();
         await handleLogin({email, password})
-        navigate('/')
+        navigate("/")
     }
 
     if(loading){
--- FRONTEND/src/features/auth/pages/Register.jsx
+++ FRONTEND/src/features/auth/pages/Register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         await handleRegister({username, email, password})
-        navigate("/")
+        navigate("/login")
     }
 
     if(loading){
